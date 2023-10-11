dd6f33c170ea23eb8ff3b9552e80a57d7f6cfedf
barnetthan <barnett.han@gmail.com>
2023-10-10 21:40:02 -0500

    added firebase

--- octoplanner/frontend/src/index.js
+++ octoplanner/frontend/src/index.js
@@ -11,9 +11,6 @@ import CalendarPage from './pages/CalendarPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
   <BrowserRouter>
       <Routes>
         <Route index element={<HomePage />} />
