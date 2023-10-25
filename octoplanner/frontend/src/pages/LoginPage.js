b442a513637caae308694e35a5d8de0e3d497af8
druthipalle <druthi.palle@gmail.com>
2023-10-24 21:54:28 -0500

    login page

--- octoplanner/frontend/src/pages/LoginPage.js
+++ octoplanner/frontend/src/pages/LoginPage.js
@@ -2,6 +2,7 @@ import { Outlet, Link, useNavigate } from "react-router-dom";
 import {db,auth} from "../api/Firebase"
 import { GoogleAuthProvider,signInWithPopup} from "firebase/auth";
 import '../css/LoginPage.css';
+import GoogleLogo from '../images/google.png';
 import NavBar from "../components/NavBar";
 
 const LoginPage = () => {
@@ -27,11 +28,32 @@ const LoginPage = () => {
 
     return (
         <div>
-            <NavBar/>
-            <div className="LoginPrompt">
-                <h1 >log in to octomize.</h1>
-                <button className="loginButton" onClick={signInWithGoogle}>Sign in with Google</button>
-                <p>Do not violate the community guidelines or you will be banned for life!</p>
+            <div class="login-box">
+                <div class="login">
+                    <h2>log in to octomize.</h2>
+                </div>
+                <div class="container">
+                    <label for="uname"><b>Email Address</b></label>
+                    <input type="text" placeholder="Enter Email Address" name="uname" required></input>
+
+                    <label for="psw"><b>Password</b></label>
+                    <input type="password" placeholder="Enter Password" name="psw" required></input>
+                
+                    <label>
+                        <input type="checkbox" name="remember"></input> Remember me
+                    </label>
+                    
+                    <span class="psw"><a href="#">Forgot Password?</a></span>
+
+                    <label class="sign-in-button">
+                        <button class = "login-button" type="submit">Sign In</button>
+                    </label>
+                </div>
+
+                <div class="google-button-container">
+                    <label>OR USE</label>
+                    <img class="google-logo-button" onClick={signInWithGoogle} src={GoogleLogo} alt="Google Logo"/>
+                </div>
             </div>
         </div>
     );
